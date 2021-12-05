f807b0f01c37f449f137da8c022db67ce94ee458
Alexey <alexgavrilov28@gmail.com>
2021-12-05 22:29:20 +0300

    Added page load time

--- scripts/load_time.js
+++ scripts/load_time.js
@@ -1,7 +1,9 @@
-var before_load_time = new Date().getTime();
-window.onload = page_load_time;
+(function() {
+    var before_load_time = new Date().getTime();
+    window.onload = page_load_time;
 
-function page_load_time() {
-    document.getElementById("load-time").innerHTML =
-        "Page load time is <b>" + (new Date().getTime() - before_load_time) / 1000 + "</b> seconds";
-}
+    function page_load_time() {
+        document.getElementById("load-time").innerHTML =
+            "Page load time is <b>" + (new Date().getTime() - before_load_time) / 1000 + "</b> seconds";
+    }
+})();
