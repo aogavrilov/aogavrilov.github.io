87bc45455e113c594c48055c814102591c7ccab6
Alexey <alexgavrilov28@gmail.com>
2021-12-20 16:40:28 +0300

    Fixed reaction on offline status

--- scripts/api.js
+++ scripts/api.js
@@ -2,12 +2,18 @@ async function addRecord() {
     document.querySelector('.preloader').classList.remove('empty');
     document.querySelector('.error').classList.add('empty');
     let url = `https://jsonplaceholder.typicode.com/todos/${Math.floor(Math.random() * (199)) + 1}`;
-    let response = await fetch(url);
-    if (response.ok) {
-        let json = await response.json();
-        addTask(json['title'], "", json['completed']);
-    } else {
+    try{
+        let response = await fetch(url);
+        if (response.ok) {
+            let json = await response.json();
+            addTask(json['title'], "", json['completed']);
+        } else {
+            document.querySelector('.error').classList.remove('empty');
+        }
+    }
+    catch (e) {
         document.querySelector('.error').classList.remove('empty');
     }
+
     document.querySelector('.preloader').classList.add('empty');
 }
